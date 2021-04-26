3db6e66186cbb6d8f74a5606c79b0007761e5adc
epm1989c7 <esteban.parra@csiete.org>
2021-04-26 13:17:43 -0500

    setup module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+install_requires = []
+
+setup(
+    name='pattern_translate',
+    version='1.0.2',
+    packages=['pattern_translate'],
+    install_requires=install_requires,
+    author='Esteban Parra Mesa'
+)
